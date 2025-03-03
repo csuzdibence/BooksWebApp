b1a738cebe4ff1ef3b28969f8b845d7ccd06e68f
User <User@23TANAR>
2025-03-03 10:39:23 +0100

    Add project files.

---
+++ Controllers/BookController.cs
@@ -0,0 +1,41 @@
+﻿using BooksWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksWebApp.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BooksDbContext dbContext;
+
+        public BookController(BooksDbContext dbContext)
+        {
+            this.dbContext = dbContext;    
+        }
+
+        // 1. végpont -> javascriptből lesz hívva AJAXon kersztül
+        [HttpGet("api/GetBookTitles")]
+        public List<BookTitleDto> GetBookTitles()
+        {
+            return dbContext.Books
+                .Select(book => new BookTitleDto()
+                {
+                    Id = book.Id,
+                    Title = book.Title
+                }).ToList();
+        }
+
+        // 2. végpont
+        [HttpGet("api/GetBookById/{id}")]
+        public Book GetBookById(int id)
+        {
+            return dbContext.Books.First(book => book.Id == id);
+        }
+    }
+
+    // Data Transfer Object
+    public class BookTitleDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ Models/BooksDbContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BooksWebApp.Models
+{
+    public class BooksDbContext : DbContext
+    {
+        public BooksDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Connection string
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=BooksDatabase");
+        }
+
+        // Kapcsolatok és Seeding beállítás
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Seedelés
+            modelBuilder.Entity<Book>().HasData
+                (
+                    new Book() 
+                    {
+                        Id = 1,
+                        Title = "American Gods",
+                        Author = "Neil Gaiman",
+                        ReleaseDate = DateTime.Parse("2016-08-16")
+                    },
+                    new Book()
+                    {
+                        Id = 2,
+                        Title = "Dune",
+                        Author = "Frank Herbert",
+                        ReleaseDate = DateTime.Parse("1965-08-01")
+                    },
+                    new Book()
+                    {
+                        Id = 3,
+                        Title = "The Da Vinci Code",
+                        Author = "Dan Brown",
+                        ReleaseDate = DateTime.Parse("2003-03-18")
+                    }
+                );
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
